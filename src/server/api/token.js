7f7261520482eaef34b4621aac06604d5af9dfee
greg@thinktopography.com <greg@thinktopography.com>
2019-02-27 14:39:48 -0500

    eslint

--- src/server/api/token.js
+++ src/server/api/token.js
@@ -1,3 +1,4 @@
+import APIError from '../utils/error'
 import { decode } from '../lib/jwt'
 import User from '../models/user'
 
@@ -5,25 +6,35 @@ const route = async (req, res, next) => {
 
   const token = req.headers.authorization
 
-  if(!token) throw new Error('No token ')
+  if(!token) throw new APIError({
+    code: 401,
+    message: 'No token'
+  })
 
   const matches = token.match(/Bearer (.*)/)
 
-  if(!matches) throw new Error('Invalid token')
+  if(!matches) throw new APIError({
+    code: 401,
+    message: 'Invalid token'
+  })
 
   const { exp, user_id } = decode(matches[1])
 
-  console.log({ exp, user_id })
-
   const iat = Math.floor(Date.now() / 1000)
 
-  if(iat > exp) throw new Error('Expired token')
+  if(iat > exp) throw new APIError({
+    code: 401,
+    message: 'Expired token'
+  })
 
   const user = await User.where({
     id: user_id
   }).fetch()
 
-  if(!user) throw new Error('Invalid user')
+  if(!user) throw new APIError({
+    code: 401,
+    message: 'Invalid user'
+  })
 
   req.user = user
 
