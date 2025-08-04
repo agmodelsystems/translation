eb922092c254fa0f81b53b4659eb9762ebb7b980
MounikaRamakkagariAMTS <mounika@agmodelsystems.com>
2025-08-04 11:43:19 -0400

    ssl config check

--- src/server/lib/knex.js
+++ src/server/lib/knex.js
@@ -2,19 +2,20 @@ import Knex from 'knex'
 
 const knex = Knex({
   client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl:{
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-  
+  connection: process.env.DATABASE_URL,
   useNullAsDefault: true,
   pool: {
     min: 3,
     max: 5
   }
 })
+
+/*connection: {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
+},*/
 
 export default knex
