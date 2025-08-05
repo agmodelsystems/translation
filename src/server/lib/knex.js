1a50f9ca73232be69d7526e6edda945a9bebd5ab
MounikaRamakkagariAMTS <mounika@agmodelsystems.com>
2025-08-05 09:27:52 -0400

    knex DB config change

--- src/server/lib/knex.js
+++ src/server/lib/knex.js
@@ -3,24 +3,21 @@ import Knex from 'knex'
 const knex = Knex({
   client: 'pg',
   connection: {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-},
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   useNullAsDefault: true,
   pool: {
     min: 3,
     max: 5
   }
 })
-
-/*connection: {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false
-  }
-},*/
 
 export default knex
